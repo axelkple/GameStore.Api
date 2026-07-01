74427bbb340145f5144144fe7ac86f780627eb14
axel kple <axelkple@gmial.com>
2026-06-30 21:28:40 -0400

    first migrations

--- obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
+++ obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dfb08e0efa52f93004a9308c23e2cadd8c7bb48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c896ee7a61765cab1f1166a3a33ef2b04e0cf1d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
